3b52f31c799c851895ccc8a7d43d1f9f8b86c57e
forkanju <forkanju44rth@gmail.com>
2026-07-15 09:08:20 +0600

    fix app launcher icon background color issue

--- composeApp/src/commonMain/kotlin/ngo/friendship/mhealth/dc/domain/network/NetworkConfig.kt
+++ composeApp/src/commonMain/kotlin/ngo/friendship/mhealth/dc/domain/network/NetworkConfig.kt
@@ -3,10 +3,10 @@ package ngo.friendship.mhealth.dc.domain.network
 import ngo.friendship.mhealth.dc.isDebugBuild
 
 object NetworkConfig {
-    const val BASE_URL_PROD = "http://devs2.apps.friendship.ngo:8080"
-    const val BASE_URL_DEV = "http://devs2.apps.friendship.ngo:8080"
-//    const val BASE_URL_DEV = "http://10.11.9.49:8080"
-//    const val BASE_URL_PROD = "http://10.11.9.49:8080"
+//    const val BASE_URL_PROD = "http://devs2.apps.friendship.ngo:8080"
+//    const val BASE_URL_DEV = "http://devs2.apps.friendship.ngo:8080"
+    const val BASE_URL_DEV = "http://10.11.9.49:8080"
+    const val BASE_URL_PROD = "http://10.11.9.49:8080"
 
     fun getBaseUrl(): String =
         if (isDebugBuild) BASE_URL_DEV else BASE_URL_PROD
